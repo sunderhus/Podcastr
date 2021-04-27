1a5d26ca5d7b1b84cc457a02efc9cfb6009c200e
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2021-04-27 11:16:58 -0300

    feat: add env-cmd to choose arbitrary env files

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript, } from 'next/document';
 
 
 
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                    <title>PoadCastr - {process.env.REACT_APP_CLIENT_NAME}</title>
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
                 </Head>
